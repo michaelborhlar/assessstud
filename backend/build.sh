8873eed162723621406af7a1880c5d080426d0c0
nikehcodes <adefehinti1@gmail.com>
2026-05-10 23:14:51 +0100

    made few changes

--- backend/build.sh
+++ backend/build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit
+
+pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
+
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
