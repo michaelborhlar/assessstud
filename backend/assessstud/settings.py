edb4380739de527e5680863ca1caf6b25ad90b5c
michaelborhlar <michealadefehinti09@gmail.com>
2026-05-13 09:11:23 +0100

    Configure CORS settings in settings.py
    Enable CORS credentials and specify allowed headers.

--- backend/assessstud/settings.py
+++ backend/assessstud/settings.py
@@ -80,9 +80,23 @@ SIMPLE_JWT = {
 }
 
 CORS_ALLOWED_ORIGINS = os.environ.get(
-    'CORS_ALLOWED_ORIGINS', 'http://localhost:5173'
+    'CORS_ALLOWED_ORIGINS',
+    'http://localhost:5173'
 ).split(',')
 
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'Africa/Lagos'
 USE_I18N = True
