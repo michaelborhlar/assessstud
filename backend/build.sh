d4dba6126de533533559621ea656b22e9d2d6101
michaelborhlar <michealadefehinti09@gmail.com>
2026-05-24 18:31:45 +0100

    Update migration command to run with syncdb

--- backend/build.sh
+++ backend/build.sh
@@ -6,5 +6,5 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py makemigrations
-python manage.py migrate
+python manage.py migrate --run-syncdb
 python manage.py seed_classes
