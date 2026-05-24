e6c7de5271a4eca712b5b171273e0494a6304aeb
michaelborhlar <michealadefehinti09@gmail.com>
2026-05-24 18:50:34 +0100

    Remove specific makemigrations commands from build script

--- backend/build.sh
+++ backend/build.sh
@@ -5,8 +5,5 @@ pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py makemigrations accounts
-python manage.py makemigrations
-python manage.py migrate accounts
 python manage.py migrate
 python manage.py seed_classes
