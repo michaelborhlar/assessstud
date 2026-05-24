196df54fb45019a50f93294e52887a42d976074c
michaelborhlar <michealadefehinti09@gmail.com>
2026-05-24 18:37:53 +0100

    Update migration commands in build script

--- backend/build.sh
+++ backend/build.sh
@@ -5,6 +5,8 @@ pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+python manage.py makemigrations accounts
 python manage.py makemigrations
-python manage.py migrate --run-syncdb
+python manage.py migrate accounts
+python manage.py migrate
 python manage.py seed_classes
