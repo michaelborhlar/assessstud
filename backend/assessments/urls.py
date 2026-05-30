bfdb45ebdb6ccfc63516fecdb9fdf7a0c0e3d336
nikehcodes <adefehinti1@gmail.com>
2026-05-30 14:49:01 +0100

    made few corrections

--- backend/assessments/urls.py
+++ backend/assessments/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('questions/', views.QuestionListCreateView.as_view()),
     path('questions/bulk-upload/', views.BulkUploadQuestionsView.as_view()),
     path('', views.AssessmentListCreateView.as_view()),
-    path('<int:assessment_id>/submissions/', views.AssessmentSubmissionsView.as_view()),
     path('answers/<int:answer_id>/review/', views.AdminReviewAnswerView.as_view()),
     path('questions/<int:question_id>/delete/', views.QuestionDeleteView.as_view()),
     path('<int:assessment_id>/delete/', views.AssessmentDeleteView.as_view()),
@@ -17,4 +16,5 @@ urlpatterns = [
     path('<int:assessment_id>/start/', views.StartAssessmentView.as_view()),
     path('session/<int:session_id>/submit/', views.SubmitAssessmentView.as_view()),
     path('my/overall/', views.StudentOverallScoreView.as_view()),
+    path('<int:assessment_id>/submissions/', views.AssessmentSubmissionsView.as_view()),
 ]
